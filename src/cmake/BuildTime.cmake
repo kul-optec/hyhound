ef34f22751bff939f99b7c8bd0a1479515b8b6c1
Pieter Pas <pieter.p.dev@outlook.com>
2025-09-22 11:45:55 +0200

    Improve commit hash packaging

--- src/cmake/BuildTime.cmake
+++ src/cmake/BuildTime.cmake
@@ -10,6 +10,15 @@ else()
         OUTPUT_VARIABLE HYHOUND_COMMIT_HASH
         OUTPUT_STRIP_TRAILING_WHITESPACE
         ERROR_QUIET)
+    execute_process(
+        COMMAND git status . --short --no-branch --untracked-files=no
+        WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+        OUTPUT_VARIABLE HYHOUND_GIT_STATUS
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        ERROR_QUIET)
+    if (NOT HYHOUND_GIT_STATUS STREQUAL "" AND NOT HYHOUND_COMMIT_HASH STREQUAL "")
+        string(APPEND HYHOUND_COMMIT_HASH "-dirty")
+    endif()
 endif()
 configure_file(${CMAKE_CURRENT_LIST_DIR}/hyhound-build-time.cpp.in
     hyhound-build-time.cpp @ONLY)
