42a4b5c0c28b2ca237982197d54851054b584a80
Pieter Pas <pieter.p.dev@outlook.com>
2025-10-23 15:44:10 +0200

    Version 1.0.2a2

--- python/hyhound/__init__.py
+++ python/hyhound/__init__.py
@@ -2,7 +2,7 @@
 Hyperbolic Householder transformations for Up- and Downdating Cholesky factorizations.
 """
 
-__version__ = "1.0.2a1"
+__version__ = "1.0.2a2"
 
 from ._hyhound import *  # noqa: F403
 from ._hyhound import __version__ as __c_version__  # noqa: F401
